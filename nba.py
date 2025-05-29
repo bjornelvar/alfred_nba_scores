2d6be5d8e8d15b28742d73215a71d76b9ba09961
Björn Elvar <b@bjossi.dev>
2025-05-28 19:22:30 -0500

    timezone fix and player name fix

--- nba.py
+++ nba.py
@@ -23,6 +23,13 @@ def fetch_games():
 
     return data["scoreboard"]["games"]
 
+def fix_mojibake(s: str) -> str:
+    # Fixing stupid mojibake in the response
+    try:
+        return s.encode('latin-1').decode('utf-8')
+    except (UnicodeEncodeError, UnicodeDecodeError):
+        return s
+
 def format_games(games):
     items = []
     for game in games:
@@ -30,8 +37,7 @@ def format_games(games):
         home = game["homeTeam"]
         leaders = game["gameLeaders"]
 
-        # parse UTC and convert to local
-        utc_dt = datetime.strptime(game["gameTimeUTC"], "%Y-%m-%dT%H:%M:%SZ") \
+        utc_dt = datetime.strptime(game["gameTimeUTC"], "%Y-%m-%dT%H:%M:%SZ")\
                       .replace(tzinfo=timezone.utc)
         local_dt = utc_dt.astimezone()
         gametime = local_dt.strftime("%H:%M %Z")
@@ -39,15 +45,25 @@ def format_games(games):
         away_code, home_code, gid = away["teamTricode"], home["teamTricode"], game["gameId"]
 
         if game["gameStatus"] == 1:
-            title  = f"{away_code} @ {home_code}"
-            subtitle = gametime
+            # If playoff game, display series info
+            # Playoff game will have something in this field instead of "", playin games will have "" as well as regular season games
+            if game["seriesGameNumber"] != "":
+                subtitle = f"{gametime} - {game['seriesGameNumber']} ({game['seriesText']})"
+            else:
+                # Just put game time if not a playoff game
+                subtitle = f"{gametime} - {away_code} {away['wins']}-{away['losses']} {home_code} {home['wins']}-{home['losses']}"
+            title = f"{away_code} @ {home_code}"
             arg = f"{BASE_URL}{away_code}-vs-{home_code}-{gid}"
         else:
-            title = f"{away_code} {away['score']} - {home['score']} {home_code} ({status_text})"
+            title  = f"{away_code} {away['score']} - {home['score']} {home_code} ({status_text})"
             al, hl = leaders["awayLeaders"], leaders["homeLeaders"]
+
+            # recover any mojibake in the player names
+            al_name = fix_mojibake(al['name'])
+            hl_name = fix_mojibake(hl['name'])
             subtitle = (
-                f"{al['name']}: {al['points']}pts/{al['rebounds']}reb/{al['assists']}ast - "
-                f"{hl['name']}: {hl['points']}pts/{hl['rebounds']}reb/{hl['assists']}ast"
+                f"{al_name}: {al['points']}pts/{al['rebounds']}reb/{al['assists']}ast - "
+                f"{hl_name}: {hl['points']}pts/{hl['rebounds']}reb/{hl['assists']}ast"
             )
             arg = f"{BASE_URL}{away_code}-vs-{home_code}-{gid}/box-score"
 
--- test_nba.py
+++ test_nba.py
@@ -5,9 +5,18 @@ from datetime import datetime, timezone
 
 BASE_URL = "https://www.nba.com/game/"
 
+def fix_mojibake(s: str) -> str:
+    # Fixing stupid mojibake in the response
+    try:
+        return s.encode('latin-1').decode('utf-8')
+    except (UnicodeEncodeError, UnicodeDecodeError):
+        return s
+
+
 def fetch_games():
     # read from a local JSON file instead of hitting the API
-    path = "/Users/bjossi/GitHub/alfred_nba_scores/testing_data.json"
+    path = "/Users/bjossi/GitHub/alfred_nba_scores/data/games.json"
+    path = "/Users/bjossi/GitHub/alfred_nba_scores/data/01_nostart.json"
     try:
         with open(path, 'r', encoding='utf-8') as f:
             data = json.load(f)
@@ -32,15 +41,25 @@ def format_games(games):
         away_code, home_code, gid = away["teamTricode"], home["teamTricode"], game["gameId"]
 
         if game["gameStatus"] == 1:
-            title    = f"{away_code} @ {home_code}"
-            subtitle = gametime
-            arg      = f"{BASE_URL}{away_code}-vs-{home_code}-{gid}"
+            # If playoff game, display series info
+            # Playoff game will have something in this field instead of "", playin games will have "" as well as regular season games
+            if game["seriesGameNumber"] != "":
+                subtitle = f"{gametime} - {game['seriesGameNumber']} ({game['seriesText']})"
+            else:
+                # Just put game time if not a playoff game
+                subtitle = f"{gametime} - {away_code} {away['wins']}-{away['losses']} {home_code} {home['wins']}-{home['losses']}"
+            title = f"{away_code} @ {home_code}"
+            arg = f"{BASE_URL}{away_code}-vs-{home_code}-{gid}"
         else:
-            title    = f"{away_code} {away['score']} - {home['score']} {home_code} ({status_text})"
-            al, hl   = leaders["awayLeaders"], leaders["homeLeaders"]
+            title  = f"{away_code} {away['score']} - {home['score']} {home_code} ({status_text})"
+            al, hl = leaders["awayLeaders"], leaders["homeLeaders"]
+
+            # recover any mojibake in the player names
+            al_name = fix_mojibake(al['name'])
+            hl_name = fix_mojibake(hl['name'])
             subtitle = (
-                f"{al['name']}: {al['points']}pts/{al['rebounds']}reb/{al['assists']}ast - "
-                f"{hl['name']}: {hl['points']}pts/{hl['rebounds']}reb/{hl['assists']}ast"
+                f"{al_name}: {al['points']}pts/{al['rebounds']}reb/{al['assists']}ast - "
+                f"{hl_name}: {hl['points']}pts/{hl['rebounds']}reb/{hl['assists']}ast"
             )
             arg = f"{BASE_URL}{away_code}-vs-{home_code}-{gid}/box-score"
 
